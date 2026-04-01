995fbd08c9f95862de1f3c0adfe4e8f98b412ec8
James Moscola <jmoscola@ycp.edu>
2026-03-31 22:57:29 -0400

    Added missing dependency to fix broken 'bulk registration' feature.

--- PygmyMarmosetControllers/src/main/java/edu/ycp/cs/pygmymarmoset/app/controller/BulkRegistrationController.java
+++ PygmyMarmosetControllers/src/main/java/edu/ycp/cs/pygmymarmoset/app/controller/BulkRegistrationController.java
@@ -9,7 +9,7 @@ package edu.ycp.cs.pygmymarmoset.app.controller;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,7 +32,7 @@ public class BulkRegistrationController {
 	public List<BulkRegistrationOutcome> execute(Course course, InputStream in, SectionNumber secNum) throws IOException {
 		List<BulkRegistrationOutcome> result = new ArrayList<>();
 		
-		InputStreamReader reader = new InputStreamReader(in, Charset.forName("UTF-8"));
+		InputStreamReader reader = new InputStreamReader(in, StandardCharsets.UTF_8);
 		Iterable<CSVRecord> records = CSVFormat.DEFAULT.parse(reader);
 		int count = 0;
 		int create = 0;
